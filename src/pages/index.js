9509b4747c524a7513de7b5b6f86694c80c1063e
Salvydas Lukosius <sall@w-ss.io>
2021-12-30 11:43:28 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Z-Shell ${siteConfig.title} | Wiki Pages`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
