0d8e3e2143248c0eae9b9d67c935d2d35db4508f
Salvydas Lukosius <sall@w-ss.io>
2022-01-30 13:59:56 +0000

    WIP

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,12 +3,12 @@ const math = require("remark-math");
 const katex = require("rehype-katex");
 const npm2yarn = require("@docusaurus/remark-plugin-npm2yarn");
 const TwitterSvg =
-	'<svg style="fill: #1DA1F2; vertical-align: middle;" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>';
+	'<svg style="fill: #1DA1F2; vertical-align: middle;" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
 	url: "https://z-shell.pages.dev",
 	title: "❮ ZI ❯",
-	tagline: "A Swiss Army Knife for Zsh - Unix shell",	
+	tagline: "A Swiss Army Knife for Zsh - Unix shell",
 	baseUrl: "/",
 	titleDelimiter: "|",
 	organizationName: "z-shell",
@@ -20,33 +20,34 @@ const config = {
 	staticDirectories: ["static"],
   themes: ['live-codeblock'],
 	plugins: [
-		    ["@docusaurus/plugin-pwa",
-      {
-        debug: true,
-        offlineModeActivationStrategies: [
-          'appInstalled',
-          'standalone',
-          'queryString',
-        ],
-        pwaHead: [
-          {
-            tagName: 'link',
-            rel: 'icon',
-            href: '/img/logo.png',
-          },
-          {
-            tagName: 'link',
-            rel: 'manifest',
-            href: '/manifest.json',
-          },
-          {
-            tagName: 'meta',
-            name: 'theme-color',
-            content: 'rgb(37, 194, 160)',
-          },
-        ],
-      },
-    ],
+		[
+			"@docusaurus/plugin-pwa",
+			{
+				debug: true,
+				offlineModeActivationStrategies: [
+					"appInstalled",
+					"standalone",
+					"queryString",
+				],
+				pwaHead: [
+					{
+						tagName: "link",
+						rel: "icon",
+						href: "/img/logo.png",
+					},
+					{
+						tagName: "link",
+						rel: "manifest",
+						href: "/manifest.json",
+					},
+					{
+						tagName: "meta",
+						name: "theme-color",
+						content: "rgb(37, 194, 160)",
+					},
+				],
+			},
+		],
 	],
 	i18n: {
 		defaultLocale: "en",
@@ -54,7 +55,7 @@ const config = {
 	},
 	scripts: [
 		{
-			src: "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
+			src: "https://crowdin.com/js/crowdjet/crowdjet.js",
 			async: true,
 		},
 	],
@@ -68,14 +69,14 @@ const config = {
 				},
 				debug: true,
 				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-        	editUrl: ({locale, docPath}) => {
-        	if (locale !== 'en') {
-      	  	return `https://crowdin.com/project/z-shell-zi/${locale}`;
-    	    		}
-  	        		return `https://github.com/z-shell/zw/tree/main/`;
-	        		},
-					/**editUrl: "https://github.com/z-shell/zw/tree/main/",*/
+				sidebarPath: require.resolve("./sidebars.js"),
+        editUrl: ({locale, versionDocsDirPath, docPath}) => {
+         		if (locale !== 'en') {
+      	  		return `https://crowdin.com/project/z-shell-zi/${locale}`;
+    	    	},
+  	      	return `https://github.com/z-shell/zw/tree/main/${versionDocsDirPath}/${docPath}`;
+	        },
+						/**editUrl: "https://github.com/z-shell/zw/tree/main/",
 					editLocalizedFiles: false,
 					showLastUpdateTime: true,
 					showLastUpdateAuthor: true,
@@ -85,7 +86,7 @@ const config = {
 				pages: {
 					remarkPlugins: [math, [npm2yarn, {sync: true}]],
 					rehypePlugins: [katex],
-					},
+				},
 				blog: {
 					editUrl: "https://github.com/z-shell/zw/tree/main/",
 					editLocalizedFiles: false,
@@ -102,10 +103,10 @@ const config = {
 					trackingID: "G-46BGZ6PZR4",
 					anonymizeIP: true,
 				},
-					googleAnalytics: {
+				/**googleAnalytics: {
 					trackingID: "G-42QP74YV1E",
 					anonymizeIP: true,
-				},
+				},*/
 			}),
 		],
 	],
