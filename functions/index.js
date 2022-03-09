80a27a13a32c6f4a1156d4365fbf82c43a846a91
Salvydas Lukosius <sall@w-ss.io>
2022-03-09 07:59:44 +0000

    wrangler

--- functions/index.js
+++ functions/index.js
@@ -10,42 +10,26 @@ import {getAssetFromKV, mapRequestToAsset} from '@cloudflare/kv-asset-handler';
 const DEBUG = false;
 
 addEventListener('fetch', (event) => {
-  try {
-    event.respondWith(handleEvent(event));
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      );
-    }
-    event.respondWith(new Response('Internal Error', {status: 500}));
-  }
+  event.respondWith(handleEvent(event));
 });
 
 async function handleEvent(event) {
-  const url = new URL(event.request.url);
   let options = {};
 
   /**
    * You can add custom logic to how we fetch your assets
    * by configuring the function `mapRequestToAsset`
    */
-
-  //options.mapRequestToAsset = handlePrefix(/^\/assets/);
-  //options.mapRequestToAsset = handlePrefix(/^\/zw/);
-  //options.mapRequestToAsset = handlePrefix(/^\/img/);
-  //options.mapRequestToAsset = handlePrefix(/^\/ja/);
-  //options.mapRequestToAsset = handlePrefix(/^\/ru/);
+  //options.mapRequestToAsset = handlePrefix(/^\/assets/)
+  //options.mapRequestToAsset = handlePrefix(/^\/img/)
 
   try {
     if (DEBUG) {
-      // customize caching
       options.cacheControl = {
         bypassCache: true,
       };
     }
+
     const page = await getAssetFromKV(event, options);
 
     // allow headers to be altered
