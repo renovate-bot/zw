8c456075bc66d506542f41f40b131c94df97c1cb
Salvydas Lukosius <sall@w-ss.io>
2022-03-11 05:28:32 +0000

    ci(fix)

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,14 +1,32 @@
-import {handleRequest} from './Handler';
+import {getAssetFromKV, NotFoundError, MethodNotAllowedError} from '@cloudflare/kv-asset-handler';
+import manifestJSON from '__STATIC_CONTENT_MANIFEST';
+const assetManifest = JSON.parse(manifestJSON);
 
-export async function onRequest({request, env}: EventContext<Env, string, unknown>) {
-  const url = new URL(request.url);
-  if (url.pathname.startsWith('/assets/')) {
-    return env.ASSETS.fetch(request);
-  }
-
-  return handleRequest(request);
-}
-
-interface Env {
-  ASSETS: Fetcher;
-}
+export default {
+  async fetch(request, env, ctx) {
+    if (request.url.includes('/docs')) {
+      try {
+        return await getAssetFromKV(
+          {
+            request,
+            waitUntil(promise) {
+              return ctx.waitUntil(promise);
+            },
+          },
+          {
+            ASSET_NAMESPACE: env.__STATIC_CONTENT,
+            ASSET_MANIFEST: assetManifest,
+          },
+        );
+      } catch (e) {
+        if (e instanceof NotFoundError) {
+          // ...
+        } else if (e instanceof MethodNotAllowedError) {
+          // ...
+        } else {
+          return new Response('An unexpected error occurred', {status: 500});
+        }
+      }
+    } else return fetch(request);
+  },
+};
