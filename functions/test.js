6aab1c72dc97441e9944bb6ae36b99a33682b246
Salvydas Lukosius <sall@w-ss.io>
2022-03-11 06:05:01 +0000

    Funtioncs (#251)
    * rebase
    
    * handler
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * workers(updates) (#252)

---
+++ functions/test.js
@@ -0,0 +1,12 @@
+async function errorHandler(context) {
+  try {
+    // wait for the next function to finish
+    return await context.next();
+  } catch (err) {
+    // catch and report and errors when running the next function
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+  }
+}
+
+// Attach `errorHandler` to all HTTP requests
+export const onRequest = errorHandler;
